c26c6a6401310f5728facb6e549c7c65f9443cd2
spartanhero93 <spartanhero93@gmail.com>
2022-03-09 13:02:59 -0500

    small question 2 answered

--- index.js
+++ index.js
@@ -3,3 +3,16 @@
 function delay (ms) {
   return new Promise(res => setTimeout(res, ms))
 }
+
+// el: element node object
+// moves the element to the right by 100px over a duration of 1 second
+function animateRight(el) {
+  const delay = i => setTimeout(() => {el.style.left = `${i}px`}, 100 * i);
+  let count = 1
+  el.style.position = 'relative'
+  
+  while(count <= 100){
+    delay(count)
+    count++
+  }
+}
